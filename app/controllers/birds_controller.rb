68f7588271be18b0c12410dc63188f750814a542
Teklu Mezgebo <mezge002@umn.edu>
2023-07-30 23:11:04 -0500

    added colon

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,7 +9,7 @@ class BirdsController < ApplicationController
         bird = Bird.find(params[:id])
         render json: bird
     rescue ActiveRecord::RecordNotFound
-        render json : "Bird not found", status: :not_found
+        render json: "Bird not found", status: :not_found
     end
     
 end
